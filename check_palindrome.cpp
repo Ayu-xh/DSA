3a971db531c4e1663ccb771c8fdd965a4b02605d
Ayush Singh <ayushsingh@Ayushs-MacBook-Air.local>
2025-12-08 21:43:53 +0530

    two pointer solution for checking palindrome

---
+++ check_palindrome.cpp
@@ -0,0 +1,21 @@
+#include <string>
+using std::string;
+
+
+class Solution {
+  public:
+    bool isPalindrome(string& s) {
+        // code here
+        int i = 0, j=s.size()-1;
+        
+        while(i<j){
+            if (s[i]!=s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+};
