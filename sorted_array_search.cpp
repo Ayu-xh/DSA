e76f64ee9f1be8c9734b8574ddf92db8aedc16e1
Ayush Singh <ayushsingh90935@gmail.com>
2025-12-10 22:58:25 +0530

    Linear Search implementation

---
+++ sorted_array_search.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    bool searchInSorted(vector<int>& arr, int k) {
+        // code here
+        for (int i=0; i<arr.size(); i++){
+            if (arr[i]==k){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
