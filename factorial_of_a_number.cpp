63d83ff6a965808b38d284b493a8cdebe1f6f840
Ayush Singh <ayushsingh90935@gmail.com>
2025-12-14 14:10:01 +0530

    factorial of a number

---
+++ factorial_of_a_number.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int factorial(int n) {
+        // code here
+        if(n==1){
+            return 1;
+        }
+        
+        else{
+            return n*factorial(n-1);
+        }
+    }
+};
