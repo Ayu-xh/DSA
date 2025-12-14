8a527075eeeda8786dbccf1cfe41dcda9827e238
Ayush Singh <ayushsingh90935@gmail.com>
2025-12-14 13:48:23 +0530

    swap ones and zeroes

---
+++ swap_ones_and_zeroes.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// User function template for C++
+
+class Solution {
+  public:
+    void segregate0and1(vector<int> &arr) {
+        // code here
+        int i=0, j=arr.size()-1;
+        while(i<j){
+            if(arr[i]==0){
+                i++;
+            }
+            
+            if(arr[i]==1 && arr[j]==0){
+                swap(arr[i], arr[j]);
+                i++;
+                j--;
+            }
+            
+            if(arr[j]==1){
+                j--;
+            }
+        }
+    }
+};
