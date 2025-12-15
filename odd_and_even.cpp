d7e084e3bbe545edd95be7777410b5b98d5b7c04
Ayush Singh <ayushsingh90935@gmail.com>
2025-12-15 23:41:52 +0530

    odd and even

---
+++ odd_and_even.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    bool isEven(int n) {
+        // code here
+        if(n%2==0){
+            return true;
+        }
+        
+        else return false;
+    }
+};
