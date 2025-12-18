e47fabdfac667b7f36e9688ee22fb5bf464eb355
Ayush Singh <ayushsingh90935@gmail.com>
2025-12-18 22:22:40 +0530

    sum of an AP

---
+++ sum_of_an_AP.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+  public:
+    int sum_of_ap(int n, int a, int d) {
+        // Code here.
+        int last_term = a + ((n-1)*d);
+        int int_sum = a + last_term;
+        int sum = (n*int_sum)/2;
+        return sum;
+    }
+}
