ecbcdabc6dadeee94fb8fd29924fd2f26bfd92ba
Ayush Singh <ayushsingh90935@gmail.com>
2025-12-19 23:53:01 +0530

    multiplication table

---
+++ multiplication_table.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// User function Template for C++
+class Solution {
+  public:
+    vector<int> getTable(int n) {
+        // Write Your Code here
+        
+        vector<int> table;
+        for (int i=1; i<=10; i++){
+            table.push_back(i*n);
+        }
+        
+        return table;
+    }
+};
