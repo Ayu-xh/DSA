b8bb2beb9065688c62f431565432408eb7edd5f0
Ayush Singh <ayushsingh90935@gmail.com>
2025-12-19 20:07:02 +0530

    square root of a number

---
+++ square_root.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int floorSqrt(int n) {
+        // code here
+        int ans = floor(sqrt(n));
+        return ans;
+        
+    }
+};
