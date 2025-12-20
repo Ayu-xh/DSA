0367edf284767c62146b542fd06141f91336de29
Ayush Singh <ayushsingh90935@gmail.com>
2025-12-20 21:42:40 +0530

    simple interest using formula

---
+++ simple_interest.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// User function Template for C++
+
+class Solution {
+  public:
+    double simpleInterest(int P, int R, int T) {
+        // code here
+        return (P*R*T)/100;
+    }
+};
