075c67189345528b9bef8b5004d40d9b9a7cd78e
Ayush Singh <ayushsingh90935@gmail.com>
2025-12-24 23:14:24 +0530

    adding one to number represented as an array of digits

---
+++ adding_one_to_number.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> addOne(vector<int> &arr) {
+
+  
+    int carry = 1;
+
+    for(int i = arr.size() - 1; i >= 0; i--) {
+        int sum = arr[i] + carry;
+        arr[i] = sum % 10;
+        carry = sum / 10;
+    }
+
+    if(carry) {
+        arr.insert(arr.begin(), carry);
+    }
+
+    return arr;
+}
